417fc1f3266a15f0b7a8f877dbba6760d7994851
Evgeny Taychinov <helloevgenyy@gmail.com>
2024-04-01 23:17:23 +0300

    fix (in chat tg)

--- hw_4/4_3.py
+++ hw_4/4_3.py
@@ -66,7 +66,7 @@ class ProcessB():
 
 
 if __name__ == '__main__':
-    filename_stdout = "./artifacts/4_3_stdo.txt"
+    filename_stdout = "./artifacts/4_3_stdout.txt"
     
     a, b = mp.Pipe(duplex=False)
     lock = mp.Lock()
